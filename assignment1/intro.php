bd845d77fc0b0324c400a4a59999c9f0f2c9728c
Chanchal <chanchalgoutam014@gmail.com>
2026-06-13 22:31:02 +0530

    introduction to PHP

--- assignment1/intro.php
+++ assignment1/intro.php
@@ -1,4 +1,32 @@
 <?php
 
+// variables
+$MyName = "Chanchal";
+$Course = "Core PHP";
+$City = "Kapurthala";
 
+echo($MyName);
+echo("<br>");
+echo($Course);
+echo("<br>");
+echo($City);
+echo("<br>");
+
+// conditions(if..else)
+$marks = 55;
+if($marks >= 40){
+    echo("Pass <br>");
+}
+else{
+    echo("fail");
+}
+
+// Array
+$colors = ["black", "red", "voilet", "white", "mint"];
+echo($colors[2]);
+
+// loops(foreach)
+foreach($colors as $color){
+    echo("<br> $color");
+}
 ?>
