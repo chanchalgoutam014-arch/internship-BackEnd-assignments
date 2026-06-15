99e7e5389b54d5ce0e932bf88f844be6e7b70ed0
Chanchal <chanchalgoutam014@gmail.com>
2026-06-15 21:16:47 +0530

    task 1 competed

---
+++ assignment2/arrays.php
@@ -0,0 +1,13 @@
+<?php
+
+//task 1 : indexed array
+$fruits = ["Apple","Mango","Banana","Orange","Grapes"];
+
+$index = count($fruits);
+echo("Fruits in array are :<br> ");
+for($i=0; $i < $index; $i++){
+    echo("$fruits[$i] <br>");
+}
+echo("<br>");
+
+?>
